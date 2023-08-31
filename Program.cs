bf57f32c8754092e763eb26fdd29ce3d0313cba3
Admin <Admin@KEREMS-LAPTOP>
2023-08-31 09:49:36 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string RawInputStr = Console.ReadLine();
+
+            char[] ProcessedInputStr = new char[RawInputStr.Length];
+            char[] ReversedProcessedInputStr = new char[RawInputStr.Length];
+            bool succes; 
+
+            int InputLength = 0;
+
+            foreach (char Char in RawInputStr)
+            {
+                ProcessedInputStr[InputLength] = Char;
+                InputLength++;
+            }
+            
+            ReversedProcessedInputStr.CopyTo(ReversedProcessedInputStr, 0);
+            Array.Reverse(ReversedProcessedInputStr);
+
+            if (InputLength % 2 == 1)
+            {
+                InputLength--;
+            }
+
+            succes = true;
+            for (int i = 0; i < (InputLength / 2); i++)
+            {
+                if (!(ProcessedInputStr[i] == ReversedProcessedInputStr[i]))
+                {
+                    succes = false;
+                    break;
+                }
+            }
+
+            if (succes)
+            {
+                Console.WriteLine("Sayı Palindromdur");
+            }
+            else
+            {
+                Console.WriteLine("Sayı Palindrom Değildir");
+            }
+        }
+    }
+}
